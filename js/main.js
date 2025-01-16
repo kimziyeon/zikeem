e514390c72436ecbda5eecabb615ceb7b3fa9176
kimziyeon <aa40254037@gmail.com>
2025-01-16 18:47:09 +0900

    20250116

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('aaaaaaaZiKEEM')
+// console.log('aaaaaaaZiKEEM')
+let ww = window.innerWidth;
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3.5,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
