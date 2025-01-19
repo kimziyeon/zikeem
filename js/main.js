c5ba0e87efd3f8ca012a3422361ea29105a13cde
kimziyeon <aa40254037@gmail.com>
2025-01-19 16:40:30 +0900

    20250119

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 // console.log('aaaaaaaZiKEEM')
-let ww = window.innerWidth;
+let swiper;
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3.5,
-    freeMode: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
+function initSwiper() {
+    let ww = window.innerWidth;
+
+    if (swiper) {
+        swiper.destroy(true, true);
+    }
+
+
+    swiper = new Swiper(".mySwiper", {
+        slidesPerView: ww > 1024 ? 3.5 : ww > 640 ? 2.5 : 1.5,
+        spaceBetween: 20,
+        freeMode: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+}
+
+initSwiper();
+window.addEventListener("resize", initSwiper);
